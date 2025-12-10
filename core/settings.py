78c9c71d83d4a171a1212b610ae505214cf4ce8d
KevinAG97 <GodMod3@L1nux>
2025-12-10 13:37:52 -0300

    update: enable CORS for all origins in settings.py

--- core/settings.py
+++ core/settings.py
@@ -84,6 +84,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
+
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
